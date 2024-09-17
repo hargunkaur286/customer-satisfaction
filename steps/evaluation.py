454cd15a83738106cf64edd37746515f5be4eca8
Hargun Kaur <hargunkaur286@gmail.com>
2024-09-18 03:44:36 +0530

    training and evaluating the model

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -2,12 +2,36 @@ import logging
 
 import pandas as pd
 from zenml import step 
+from typing import Tuple
+from typing_extensions import Annotated 
+
+from src.evaluation import MSE, R2, RMSE 
 
 @step 
-def evaluate_model (df: pd.DataFrame) -> None:
+def evaluate_model (model: RegressorMixin,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+) -> Tuple[
+    Annotated[float, "r2_score"],
+    Annotated[float, "rmse"],
+]:
     """
     Evaluates the model on the ingested data.
     Args:
         df: the ingested data
     """
-    pass
+    try: 
+        prediction = model.predict(X_test)
+        mse_class = MSE()
+        mse = mse_class.calculate_scores(y_test, prediction)
+
+        r2_class = R2()
+        r2 = r2_class.calculate_scores(y_test, prediction)
+
+        rmse_class = RMSE()
+        rmse = rmse_class.calculate_scores(y_test, prediction)
+
+        return r2_score, rmse
+    except Exception as e:
+        logging.error("Error in evaluating model: {}".format(e))
+        raise e 
